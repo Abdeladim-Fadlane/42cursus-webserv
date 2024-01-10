dbcef8573851ec6b4ccf00ced92103b072e31aed
Abdeladim-Fadlane <Abdofadlane128@gmail.com>
2024-01-10 11:12:16 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,48 @@
+#include"webserv.hpp"
 
+int fastCGI() {
+    pid_t cgi_pid;
+    int fd = open("tools/index.html", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-#include "./Config/Parsing.hpp"
+    if (fd == -1) {
+        std::cerr << "Error opening file 'index.html'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-int main(int ac, char **av)
+    cgi_pid = fork();
+
+    if (cgi_pid == 0) {
+        dup2(fd, 1);
+        const char *args[] = {"/usr/bin/php-cgi8.2", "/home/afadlane/webserv/tools/phpinfo.php", NULL};
+        char *env[] = {NULL};
+        close(fd);
+        execve("/usr/bin/php-cgi8.2", const_cast<char* const*>(args), env);
+        perror("Error executing CGI");
+        exit(EXIT_FAILURE);
+    } else {
+        int status;
+        waitpid(cgi_pid, &status, 0);
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+        } else {
+            std::cerr << "Error executing CGI process" << std::endl;
+        }
+    }
+
+    close(fd);
+    return fd;
+}
+
+int main()
 {
-    if (ac != 2)  return (1);
-    Conf config_file;
-    config_file.setFileName(av[1]);
-    config_file.parse();
-    // config_file.print_all_data();
-
+    Method method;
+    try
+    {
+        multiplexing(method);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 }
