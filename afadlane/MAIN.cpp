2dafa293301f29549bb190508b18bd256a69d25f
Abdeladim-Fadlane <abdofadlane128@gmail.com>
2024-01-27 14:32:44 +0100

    27 Jan 02:32:44

--- afadlane/MAIN.cpp
+++ afadlane/MAIN.cpp
@@ -1,10 +1,12 @@
 #include"webserv.hpp"
+#include "../akatfi/parsinfCon/Configfile.hpp"
 
-
-int main()
+int main(int ac, char **av)
 {
     try
     {
+        ConfigFile config(av[1]);
+        config.parceConfig();
         multiplexing();
     }
     catch(const std::exception& e)
